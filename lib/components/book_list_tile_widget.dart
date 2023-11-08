fa70ba1c4d31fe4d3e1558f63bda0a2cb6403712
Marcelo Maciel <marselo.maciel@gmail.com>
2023-11-07 21:04:47 -0300

    book cards styles

--- lib/components/book_list_tile_widget.dart
+++ lib/components/book_list_tile_widget.dart
@@ -26,11 +26,29 @@ class _BookListTileWidgetState extends State<BookListTileWidget> {
           ),
         );
       },
-      child: ListTile(tileColor: Colors.white,
+      child: ListTile(
+        tileColor: Colors.white,
         title: Card(
           color: Colors.white,
+          shadowColor: Colors.transparent,
           surfaceTintColor: Colors.white,
           margin: const EdgeInsets.fromLTRB(0.0, 2.0, 0.0, 2.0),
+          borderOnForeground: true,
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(16),
+            ),
+            side: BorderSide(
+              color: widget.book.accessInfo.viewability == 'ALL_PAGES'
+                  ? Colors.green
+                  : widget.book.accessInfo.viewability == 'PARTIAL'
+                      ? Colors.yellow
+                      : Colors.red,
+              style: BorderStyle.solid,
+              width: 1,
+            ),
+          ),
+          // shape: BeveledRectangleBorder(borderRadius: BorderRadius.only(bottomRight: Radius.circular(50))),
           child: Padding(
             padding: const EdgeInsets.all(4.0),
             child: Container(
@@ -43,6 +61,17 @@ class _BookListTileWidgetState extends State<BookListTileWidget> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  const SizedBox(width: 8),
+                  widget.book.volumeInfo.imageLinks.thumbnail.trim().isEmpty
+                      ? StandardCoverWidget(
+                          book: widget.book,
+                        )
+                      : ClipRRect(
+                          // width: 70,
+                          child: Image.network(
+                            widget.book.volumeInfo.imageLinks.thumbnail,
+                          ),
+                        ),
                   const SizedBox(width: 8),
                   Expanded(
                     child: Column(
@@ -78,16 +107,6 @@ class _BookListTileWidgetState extends State<BookListTileWidget> {
                       ],
                     ),
                   ),
-                  widget.book.volumeInfo.imageLinks.thumbnail.trim().isEmpty
-                      ? StandardCoverWidget(
-                          book: widget.book,
-                        )
-                      : ClipRRect(
-                          // width: 70,
-                          child: Image.network(
-                            widget.book.volumeInfo.imageLinks.thumbnail,
-                          ),
-                        ),
                 ],
               ),
             ),
